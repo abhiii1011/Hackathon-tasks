38e71ebfdaa20cb1f4bbb05ff3a8aa3b4e1dd4b5
Aadarsh Dangi <aadarsht0001@gmail.com>
2025-08-24 21:56:31 +0530

    day-1-aadarsh

--- day-1-aadarsh/day-1.js
+++ day-1-aadarsh/day-1.js
@@ -1,2 +1,12 @@
-console.log("hello");
-console.log("2");
+const student = {
+  name: "Aadarsh",
+  age: 21,
+  course: "Computer Science",
+  rollNo: 101
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("Roll No:", student.rollNo);
