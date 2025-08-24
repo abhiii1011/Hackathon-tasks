6acfd6e4f39814a503cb0702942f1076c3aa7ba7
Abhishek <abhisheknamdeo0019@gmail.com>
2025-08-24 22:09:06 +0530

    day-1.js

--- day-1-abhishek/day-1.js
+++ day-1-abhishek/day-1.js
@@ -1,2 +1,11 @@
-console.log("hello");
-console.log("2");
+const student = {
+  name: "Abhishek",
+  age: 19,
+  course: "Computer Science",
+
+};
+
+console.log("Student Details:");
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
